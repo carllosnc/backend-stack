cecb05a369d7db67d662ecb37abff10c9a3736fa
Carlos Costa <carllos.nc@gmail.com>
2024-12-21 22:02:09 -0300

    refactor: remove todoTable schema definition from schema.ts

--- src/db/schema.ts
+++
@@ -1,9 +0,0 @@
-import { integer, sqliteTable, text, int } from "drizzle-orm/sqlite-core";
-import { sql } from "drizzle-orm";
-
-export const todoTable = sqliteTable("todos", {
-  id: int().primaryKey({ autoIncrement: true }),
-  title: text(),
-  completed: integer({ mode: "boolean" }).default(false),
-  created_at: text().default(sql`(CURRENT_TIMESTAMP)`),
-});
