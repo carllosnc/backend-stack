f65669f021672478e8025cb335cd1379dc70f22e
Carlos Costa <carllos.nc@gmail.com>
2024-12-21 22:05:50 -0300

    feat: implement authentication routes and improve app structure

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import { Hono } from 'hono'
+import { showRoutes } from 'hono/dev'
 import todo from './resources/todo/todo.controller'
+import { betterAuthSettings } from './auth'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.json({ message: 'Hello World!' })
-})
+app.get('/', (c) => c.json({ message: 'Hello World!' }))
+app.on(["POST", "GET"], "/api/auth/**", (c) => {
+  const auth = betterAuthSettings(
+    process.env.TURSO_DATABASE_URL!,
+    process.env.TURSO_AUTH_TOKEN!
+  )
+
+  return auth.handler(c.req.raw)
+});
 
 app.route('/todos', todo)
+
+showRoutes(app, {
+  verbose: true,
+})
 
 export default app
