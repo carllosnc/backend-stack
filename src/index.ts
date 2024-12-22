9784db344565c33d7d98f49efd4e1de1a0266fcb
Carlos Costa <carllos.nc@gmail.com>
2024-12-22 15:37:45 -0300

    feat: implement CORS support for authentication routes

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,39 @@
 import { Hono } from 'hono'
-import { showRoutes } from 'hono/dev'
 import todo from './resources/todo/todo.controller'
 import { betterAuthSettings } from './auth'
+import { Bindings } from './bindings'
+import { cors } from "hono/cors";
+import { origins } from './settings/origins'
 
-const app = new Hono()
+const app = new Hono<{Bindings: Bindings }>()
 
 app.get('/', (c) => c.json({ message: 'Hello World!' }))
-app.on(["POST", "GET"], "/api/auth/**", (c) => {
+
+app.use(
+	"/api/auth/**", // or replace with "*" to enable cors for all routes
+	cors({
+		origin: [
+			...origins
+		],
+		allowHeaders: ["Content-Type", "Authorization"],
+		allowMethods: ["POST", "GET", "OPTIONS"],
+		exposeHeaders: ["Content-Length"],
+		maxAge: 600,
+		credentials: true,
+	}),
+);
+
+app.on(["POST", "GET"], "/api/auth/**", async (c) => {
   const auth = betterAuthSettings(
-    process.env.TURSO_DATABASE_URL!,
-    process.env.TURSO_AUTH_TOKEN!
+    c.env.TURSO_DATABASE_URL,
+    c.env.TURSO_AUTH_TOKEN,
   )
 
-  return auth.handler(c.req.raw)
+  const result = await auth.handler(c.req.raw)
+
+  return result
 });
 
 app.route('/todos', todo)
-
-showRoutes(app, {
-  verbose: true,
-})
 
 export default app
