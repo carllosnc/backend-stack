5bec0db554fe863b8423b00bd0f0fadc9600db08
Carlos Costa <carllos.nc@gmail.com>
2024-12-28 10:52:09 -0300

    feat: add OpenAPI specifications and documentation routes

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { betterAuthSettings } from './auth'
 import { Bindings } from './bindings'
 import { cors } from "hono/cors";
 import { origins } from './settings/origins'
+import { openAPISpecs } from 'hono-openapi';
+import { apiReference } from '@scalar/hono-api-reference';
 
 const app = new Hono<{Bindings: Bindings }>()
 
@@ -35,5 +37,34 @@ app.on(["POST", "GET"], "/api/auth/**", async (c) => {
 });
 
 app.route('/todos', todo)
+
+app.get(
+  "/openapi",
+  openAPISpecs(app, {
+    documentation: {
+      info: {
+        title: "Hono",
+        version: "1.0.0",
+        description: "ZZ-Back boilerplate",
+      },
+      servers: [
+        {
+          url: "http://localhost:8787",
+          description: "Local server",
+        },
+      ],
+    },
+  })
+);
+
+app.get(
+  "/docs",
+  apiReference({
+    theme: "deepSpace",
+    spec: {
+      url: "/openapi",
+    },
+  })
+);
 
 export default app
