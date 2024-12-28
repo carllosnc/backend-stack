481220768012232d3fdaaa1fb5a55b5655c722cf
Carlos Costa <carllos.nc@gmail.com>
2024-12-28 10:52:21 -0300

    test: update tests to check OpenAPI schema and documentation route

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -2,11 +2,17 @@ import { expect, test, describe } from 'bun:test'
 import app from '../src/index'
 
 describe('App suite', () => {
-  test('Check hello world', async () => {
-    const res = await app.request('/')
-    const body = await res.json()
+  test('Check openapi schema', async () => {
+    const res = await app.request('/openapi ')
 
     expect(res.status).toBe(200)
-    expect(body).toEqual({ message: 'Hello World!' })
+    expect(res.headers.get('content-type')).toBe('application/json')
+  })
+
+  test('Check documentation route', async () => {
+    const res = await app.request('/docs')
+
+    expect(res.status).toBe(200)
+    expect(res.headers.get('content-type')).toBe('text/html; charset=UTF-8')
   })
 })
