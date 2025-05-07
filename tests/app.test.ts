018e66405680c552548573b9d300dd4167a01339
Carlos Costa <carllos.nc@gmail.com>
2025-05-07 17:41:04 -0300

    test(app): reorder tests for documentation and OpenAPI schema checks

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -2,17 +2,17 @@ import { expect, test, describe } from 'bun:test'
 import app from '../src/index'
 
 describe('App suite', () => {
-  test('Check openapi schema', async () => {
-    const res = await app.request('/openapi ')
-
-    expect(res.status).toBe(200)
-    expect(res.headers.get('content-type')).toBe('application/json')
-  })
-
   test('Check documentation route', async () => {
     const res = await app.request('/docs')
 
     expect(res.status).toBe(200)
     expect(res.headers.get('content-type')).toBe('text/html; charset=UTF-8')
+  })
+
+  test('Check openapi schema', async () => {
+    const res = await app.request('/docs/openapi ')
+
+    expect(res.status).toBe(200)
+    expect(res.headers.get('content-type')).toBe('application/json')
   })
 })
