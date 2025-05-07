988f6d7b1aebf451e629cc49471fec7e1b9b4dba
Carlos Costa <carllos.nc@gmail.com>
2025-05-07 15:47:33 -0300

    refactor(index): reorganize routes and improve bearer authentication handling

--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,19 @@ import { betterAuthSettings } from './auth'
 import { Bindings } from './bindings'
 import { cors } from "hono/cors";
 import { origins } from './settings/origins'
-import { openAPISpecs } from 'hono-openapi';
-import { apiReference } from '@scalar/hono-api-reference';
 import { bearerAuth } from 'hono/bearer-auth'
 import { drizzle } from 'drizzle-orm/libsql';
 import { account } from './db/auth-schema';
 import { eq } from 'drizzle-orm';
+import { docs } from '@/settings/docs'
+import { showRoutes } from 'hono/dev'
 
 const app = new Hono<{Bindings: Bindings }>()
 
-app.get('/', (c) => {
-  return c.json({
-    app: "Hone stack",
-    description: "Hone stack boylerplate",
-    url: c.env.APP_URL,
-    open_api: `${c.env.APP_URL}/openapi`,
-    docs: `${c.env.APP_URL}/docs`,
-  })
-})
-
-app.use('/todos', bearerAuth({
+app.use('/res/*', bearerAuth({
+  invalidTokenMessage: "Invalid token",
+  invalidAuthenticationHeaderMessage: "Invalid authentication header",
+  noAuthenticationHeaderMessage: "No authentication header",
   verifyToken: async (token, c) => {
     const db = drizzle({ connection: {
       url: c.env.TURSO_DATABASE_URL,
@@ -31,6 +24,10 @@ app.use('/todos', bearerAuth({
     }})
 
     const accountResult = await db.select().from(account).where(eq(account.idToken, token))
+
+    if(accountResult.length === 0) {
+      return false
+    }
 
     return token === accountResult[0].idToken
   },
@@ -48,7 +45,7 @@ app.use(
 		maxAge: 600,
 		credentials: true,
 	}),
-);
+ );
 
 app.on(["POST", "GET"], "/api/auth/**", async (c) => {
   const auth = betterAuthSettings({
@@ -59,43 +56,26 @@ app.on(["POST", "GET"], "/api/auth/**", async (c) => {
   })
 
   const result = await auth.handler(c.req.raw)
-
   return result
 });
 
-app.get(
-  "/openapi",
-  openAPISpecs(app, {
-    documentation: {
-      info: {
-        title: "Hono",
-        version: "1.0.0",
-        description: "Hone stack boylerplate",
-      },
-      servers: [
-        {
-          url: process.env.APP_URL as string,
-          description: "Local server",
-        },
-      ],
-    },
-  })
-);
-
-app.get(
-  "/docs",
-  apiReference({
-    theme: "deepSpace",
-    spec: {
-      url: "/openapi",
-    },
-  })
-);
-
-/*=========*/
+/*====================*/
 // ROUTES
-/*=========*/
+/*====================*/
 
-app.route('/todos', todo)
+app.get('/', (c) => {
+  return c.json({
+    app: "Hone stack",
+    description: "Hone stack boylerplate",
+    url: c.env.APP_URL,
+    open_api: `${c.env.APP_URL}/docs/openapi`,
+    docs: `${c.env.APP_URL}/docs`,
+  })
+})
+
+app.route('/docs', docs)
+app.route('/res/v1/todos', todo)
+
+showRoutes(app)
 
 export default app
